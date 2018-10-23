8ae5871c06a192a140205b8b64eadaac041f6981
leo mangutov and Noah Schooler <leo@>
2018-10-23 12:55:46 -0700

    add JavaScript to find numbers greater than four in an array using the forEach() method

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-console.log("hi")
-  var numbers = [1, 5, 7, 2, 6, 8, 4];
+var numbers = [1, 5, 7, 2, 6, 8, 4]
 
-  function findNumsGreaterThanFour (numbers) {
-    console.log("function")
-  	var numsGreaterThanFour = [];
-  	for (var index = 0; index < numbers.length; index++) {
-      console.log("i: " + index + ", value: " + numbers[index]);
-      if (numbers[index] > 4) {
-        console.log("in if: " + numbers[index]);
-      	numsGreaterThanFour.push(numbers[index])
-      }
+function findNumbersGreaterThanFour(numbers) {
+  var numsGreaterThanFour = [];
+  numbers.forEach(function(element){
+    if (element > 4) {
+      numsGreaterThanFour.push(element);
     }
-    return numsGreaterThanFour;
-  }
+  });
+  return numsGreaterThanFour;
+}
 
-  var result = findNumsGreaterThanFour(numbers)
+$(document).ready(function() {
+  var result = findNumbersGreaterThanFour(numbers)
   $("#output").append(result);
-
 });
